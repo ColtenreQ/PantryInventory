87e20e635e5003142574765b72a65637f23d5773
Colten <finalrebellion123@gmail.com>
2023-10-16 20:01:07 -0500

    added services and a new list of items to test with.

--- src/app/pantry/food-list/food-list.component.ts
+++ src/app/pantry/food-list/food-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { PantryService } from '../pantry.service';
+
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
+
 export class FoodListComponent {
 
+  constructor(private pantryService: PantryService) {}
+
+  foodList = this.pantryService.getPantryItems();
 }
--- src/app/pantry/pantry.service.ts
+++ src/app/pantry/pantry.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@angular/core';
+import { foodItem } from '../models/foodItem.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PantryService {
+
+  pantryItems = [
+    new foodItem("Banana", 27, "Fruit"),
+    new foodItem("Beet", 38, "Vegetable"),
+    new foodItem("Rice", 120, "Grain"),
+    new foodItem("Chicken", 3, "Meat"),
+    new foodItem("Pork", 8, "Meat"),
+    new foodItem("Steak", 4, "Meat"),
+    new foodItem("Apple", 15, "Fruit"),
+  ]
+
+  getPantryItems() {
+    return this.pantryItems;
+  }
 }
